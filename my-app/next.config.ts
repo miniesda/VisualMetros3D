d7b17dda661ef792fadcd68978cd4b39a00ea5e0
David Rodriguez Rives   -   The Pirate Cat <miniesda@gmail.com>
2026-02-24 23:47:58 +0100

    quit vercel

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,8 +1,29 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/VisualMetros3D",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
   images: {
     unoptimized: true,
   },
-  reactStrictMode: true,
 };
 
 export default nextConfig;
